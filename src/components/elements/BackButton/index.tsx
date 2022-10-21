de04cb84dc1d41522f99981354a9ead7ecacc2ee
mmorales <mt.morales@hotmail.fr>
2022-10-21 18:51:37 +0200

    refactored elements directory

--- src/components/elements/BackButton/index.tsx
+++ src/components/elements/BackButton/index.tsx
@@ -4,14 +4,12 @@ const BackButton = () => {
   const router = useRouter();
 
   return (
-    <>
-      <button
-        className="block mb-6 capitalize opacity-50 hover:opacity-100 hover:text-orange transition xl:mb-14"
-        onClick={() => router.back()}
-      >
-        go back
-      </button>
-    </>
+    <button
+      className="mb-6 block capitalize opacity-50 transition hover:text-orange hover:opacity-100 xl:mb-14"
+      onClick={() => router.back()}
+    >
+      go back
+    </button>
   );
 };
 
